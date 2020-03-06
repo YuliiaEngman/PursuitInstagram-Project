c28a8d291365b5390afb8090bad9ede2ef72a2a8
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-06 13:21:05 -0500

    added DesignableView to the project

---
+++ PursuitInstagram-Project/Views/DesignableView.swift
@@ -0,0 +1,64 @@
+//
+//  DesignableView.swift
+//  PursuitInstagram-Project
+//
+//  Created by Yuliia Engman on 3/6/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class DesignableView: UIView {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableButton: UIButton {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableTextField: UITextField {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableImageView: UIImageView {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
