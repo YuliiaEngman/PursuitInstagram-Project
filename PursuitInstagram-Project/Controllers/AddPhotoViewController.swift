58cf0d0a9e2008bb93e769ac817da5efa19e602b
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-08 14:34:41 -0400

    added objects to AddPhotoViewController

--- PursuitInstagram-Project/Controllers/AddPhotoViewController.swift
+++ PursuitInstagram-Project/Controllers/AddPhotoViewController.swift
@@ -9,22 +9,15 @@
 import UIKit
 
 class AddPhotoViewController: UIViewController {
+    
+    @IBOutlet weak var photoNameLabel: UILabel!
+    @IBOutlet weak var photoImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
