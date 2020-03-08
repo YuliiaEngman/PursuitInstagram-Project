d9c043f730708e5b9ff8404ffc547c186d953e29
Yuliia Engman <yuliia.engman@gmail.com>
2020-03-08 11:21:41 -0400

    created a model for picture

---
+++ Models/PictureModel.swift
@@ -0,0 +1,29 @@
+//
+//  PictureModel.swift
+//  PursuitInstagram-Project
+//
+//  Created by Yuliia Engman on 3/8/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import Foundation
+
+struct Picture {
+    let imageURL: String
+    let pictureName: String
+    let pictureId: String
+    let listedDate: Date
+    let userNameWhoPostedPicture: String
+    let userIdWhoPostedPicture: String
+}
+
+extension Picture {
+    init(_ dictionary: [String: Any]) {
+        self.imageURL = dictionary["imageURL"] as? String ?? "no image url"
+        self.pictureName = dictionary["pictureName"] as? String ?? "no picture name"
+        self.pictureId = dictionary["pictureId"] as? String ?? "no pictureId"
+        self.listedDate = dictionary["listedDate"] as? Date ?? Date()
+        self.userNameWhoPostedPicture = dictionary["userNameWhoPostedPicture"] as? String ?? "no user name"
+        self.userIdWhoPostedPicture = dictionary["userIdWhoPostedPicture"] as? String ?? "no user ID"
+    }
+}
